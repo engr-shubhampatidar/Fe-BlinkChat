c8f0c239badd07f5b7fb26eaff8b466d6de69ada
rajpatidar00 <rajpatidar2521@gmail.com>
2024-03-30 23:06:42 +0530

    setup and fix things

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import {Home } from './pages/Home';
+import Register from './components/Auth/Register';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/home" exact element={<Home />} />
-          {/* <Route path="/login" component={Login} /> */}
-          {/* <Route path="/register" component={Register} /> */}
+          <Route path="/" exact element={<Home />} />
+          {/* <Route path="/login" component={<Login/>} /> */}
+          <Route path="/register" exact element={<Register/>} />
         </Routes>
       </Router>
     </div>
