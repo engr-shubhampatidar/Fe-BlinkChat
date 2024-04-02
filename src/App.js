1ca4e041c4f7c69e070e970e87c5105cab23559b
deepakmukati <patidardeepak072@gmail.com>
2024-04-02 14:52:00 +0530

    added axios intersepter and privateroute

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import LoginPage from "./components/Auth/Login";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Chat from "./pages/Chat";
 import Home from "./pages/Home";
-
+import PrivateRoute from "./services/PrivateRoute";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           <Route path="/" exact element={<Home />} />
           <Route path="/register" exact element={<Register />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/chats" element={<Chat />} />
+          <Route path="/chats" element={<PrivateRoute Component={Chat}/>} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
